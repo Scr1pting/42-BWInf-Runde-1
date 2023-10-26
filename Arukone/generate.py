0c350da93c2f5fc880f3c3367004e4976f989078
Jonas B <65017752+Scr1pting@users.noreply.github.com>
2023-10-26 15:17:57 +0200

    Add documentation to arukone

---
+++ Arukone/generate.py
@@ -0,0 +1,116 @@
+from utility import get_surrounding_elements
+from solve import find_path, has_path_combination
+
+import random
+
+
+def find_first_occurrence(arr: list, target: any) -> tuple:
+    """Finds the first occurrence of a target element in a list.
+
+    Args:
+        arr (list): the list that is searched
+        target (any): the searched-for element
+
+    Returns:
+        tuple: the indices of the first occurrence (row, column)
+    """
+    # Iterate over the rows of arr
+    for row_index, row in enumerate(arr):
+        # Iterate over the contents of each row
+        for col_index, value in enumerate(row):
+            if value == target:
+                # Return the indices of the first occurrence
+                return (row_index, col_index)
+
+    return None # Return None if the element is not found in the array
+
+
+def is_solvable_arukone(arukone: list, pairs: int) -> bool:
+    """Checks whether an arukone is solvable.
+
+    Note: May take very long to compute as it goes through every possible combination of paths.
+
+    Args:
+        arukone (list): a 2d array of the field. Empty squares have a value of 0
+        pairs (int): the amount of number pairs
+
+    Returns:
+        bool: whether or not an arukone is solvable.
+    """
+    all_paths = []
+
+    for pair in range(1, pairs + 1):
+        # Determine the indices of the first occurrence of a pair
+        row, col = find_first_occurrence(arukone, pair)
+        # Find paths between the two numbers of the pair
+        paths = find_path(arukone, [(pair, row, col)])
+        # Append the paths to all_paths
+        all_paths.append(paths)
+
+    # Check whether there is a valid path combination for the arukone
+    return has_path_combination(all_paths)
+
+
+def generate_arukone(field_size: int, pairs: int) -> list:
+    """Generates a solvable arukone.
+
+    Args:
+        field_size (int): number of rows and columns
+        pairs (int): number of pairs
+
+    Raises:
+        ValueError: field size too small or not enough pairs
+
+    Returns:
+        list: the solvable arukone
+    """
+    # Raise errors for invalid parameters
+    if field_size < 5:
+        raise ValueError("Parameter field_size must be greater than 4")
+    elif pairs < 2:
+        raise ValueError("Parameter pairs must be greater than 2")
+
+    # Generate empty field
+    field = [[0 for _ in range(field_size)] for _ in range(field_size)]
+
+    def fill_field(value: int):
+        """Randomly fills field with a value.
+
+        Args:
+            value (int): value to put into arukone
+        """
+        # Randomly generate location of
+        row = random.randint(0, field_size - 1)
+        col = random.randint(0, field_size - 1)
+        
+        # Check whether spot is empty and that the same number is not directly next to it
+        if field[row][col] == 0 and (value, row, col) not in get_surrounding_elements(arr=field, row=row, col=col):
+            # Fill field
+            field[row][col] = value
+        else:
+            # Regenerate value
+            fill_field(value)
+
+    # Iterate over each pair
+    for value in range(1, pairs + 1):
+        # Fill value twice into arukone
+        fill_field(value)
+        fill_field(value)
+
+    # Check whether generated arukone is solvable
+    if not is_solvable_arukone(field, pairs):
+        # Regenerate arukone if it is not solvable
+        return generate_arukone(field_size, pairs)
+
+    # Return arukone
+    return field
+
+
+if __name__ == "__main__": # Check whether generate.py runs as main program
+    # Prompt user for field size and number of pairs
+    field_size = int(input("Field size: "))
+    pairs = int(input("Number of pairs: "))
+
+    # Neatly print arukone
+    for row in generate_arukone(field_size=field_size, pairs=pairs):
+        print(' '.join(map(str, row)))
---
+++ Arukone/solve.py
@@ -0,0 +1,93 @@
+from utility import get_surrounding_elements
+
+
+def find_path(arukone: list, original_path: list) -> list:
+    """A recursive function that finds all valid paths that lead 
+    from a number to its matching number.
+
+    Args:
+        arukone (list): a 2d array of the field. Empty squares have a value of 0
+        original_path (list): the path that gets extended
+
+    Returns:
+        list: all valid paths
+    """
+    # Get the location of the last item of the path
+    _, row_last_element, col_last_element = original_path[-1]
+
+    # Get the surrounding elements 
+    surrounding_elements = get_surrounding_elements(arukone, row_last_element, col_last_element)
+    
+    paths = []
+    valid_paths = []
+
+    # Detect whether path touches itself by checking if there's more than one 
+    # common element between the original path and surrounding elements.
+    # If it does, the path is discarded. It does not provide a new solution as there is already 
+    # a path that branches of earlier, but only uses unnecessary computing resources.
+    if len(set(original_path) & set(surrounding_elements)) > 1:
+        return []
+    
+    # Iterate over each surrounding element
+    # Each surrounding element provides a part of possible solution and is therefore explored
+    for element in surrounding_elements:
+        # Get first variable of tuple
+        value = element[0]
+        
+        # Stop lines crossing over themselves
+        if element not in original_path:
+            if value == original_path[0][0]:
+                valid_paths.append(original_path + [element])
+                # Stops the path from being extended when it touches endpoint
+                return valid_paths
+            elif value == 0:
+                paths.append(original_path + [element])
+
+    # Continue extending path recursively one element at a time
+    for path in paths:
+        valid_paths += find_path(arukone, path)
+
+    # Return the completed list of valid paths for one pair
+    return valid_paths
+
+
+def has_path_combination(all_paths: list, existing_path_combination: list = [], index: int = 0) -> bool:
+    """Recursive function that checks if there is a valid combination of paths that connects
+    all pairs of numbers.
+
+    Args:
+        all_paths (list): a 3d array containing the paths of all pairs of numbers.
+        existing_path_combination (list, optional): A one-dimensional list containing the tuples of the paths of the previously already checked number pairs. Defaults to [].
+        index (int, optional): Index of the 3d array. Defaults to 0.
+
+    Returns:
+        bool: whether or not there is a valid combination of paths connecting all pairs of numbers
+    """
+    # Get a list of the paths at the provided index of the all_paths array
+    paths = all_paths[index]
+
+    # Iterate of each path 
+    for path in paths:
+        # Check that there is no overlap between the new path and the existing combination of paths
+        if len(set(path) & set(existing_path_combination)) == 0:
+            if index == len(all_paths) - 1:
+                # Last element in array
+                # Means that all previous checks succeeded, and that there is a possible solution
+                return True
+            else:
+                # Not at last check yet
+                # Continue checking for the paths that follow.
+                value = has_path_combination(
+                    all_paths=all_paths,
+                    existing_path_combination=existing_path_combination + path,
+                    index=index + 1
+                )
+                
+                # Only return a value if that value is True
+                # Otherwise, the function would return the value of the first path it examined,
+                # even if a valid combination of paths exists
+                if value == True:
+                    return True
+
+    # Default to False
+    return False
