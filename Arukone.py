911ffee2d3fbde411f943b7f7e2b4ecbb00a5e64
Jonas B <65017752+Scr1pting@users.noreply.github.com>
2023-10-26 13:40:00 +0200

    Update Arukone.py

--- Arukone.py
+++ Arukone.py
@@ -1,33 +1,5 @@
 import random
 
-def generate_arukone(field_size: int, pairs: int) -> list:
-    if field_size < 5:
-        raise ValueError("Parameter field_size must be greater than 4")
-    
-    field = [[0 for _ in range(field_size)] for _ in range(field_size)]
-
-    for i in range(0, pairs):
-        index_row_a = random.randint(0, field_size - 1)
-        index_item_a = random.randint(0, field_size - 1)
-
-        index_row_b = random.randint(0, field_size - 1)
-        index_item_b = random.randint(0, field_size - 1)
-
-        if field[index_row_a][index_item_a] == 0: 
-            field[index_row_a][index_item_a] = i + 1
-        if field[index_row_b][index_row_b] == 0:
-            field[index_row_b][index_item_b] = i + 1
-
-    return field
-
-def find_first_occurrence(arr, target):
-        for row_index, row in enumerate(arr):
-            for col_index, value in enumerate(row):
-                if value == target:
-                    return (row_index, col_index)  # Return the indices of the first occurrence
-
-        return None  # Return None if the element is not found in the array
-
 def get_surrounding_elements(arr, row: int, col: int) -> tuple:
     surrounding = []
     num_rows = len(arr)
@@ -63,6 +35,8 @@ def find_path(arukone: list, original_path: list) -> tuple:
         if element not in original_path:
             if value == original_path[0][0]:
                 valid_paths.append(original_path + [element])
+                # Stops the path from being extended when it touches endpoint
+                return valid_paths
             elif value == 0:
                 paths.append(original_path + [element])
 
@@ -72,28 +46,75 @@ def find_path(arukone: list, original_path: list) -> tuple:
     return valid_paths
 
 def find_first_occurrence(arr, target):
-        for row_index, row in enumerate(arr):
-            for col_index, value in enumerate(row):
-                if value == target:
-                    return (row_index, col_index)  # Return the indices of the first occurrence
+    for row_index, row in enumerate(arr):
+        for col_index, value in enumerate(row):
+            if value == target:
+                return (row_index, col_index)  # Return the indices of the first occurrence
 
-        return None  # Return None if the element is not found in the array
+    return None  # Return None if the element is not found in the array
 
-def solve_arukone(arukone: list, pairs: int):
+def has_path_combination(all_paths: list, valid_path: list = [], index: int = 0):
+    paths = all_paths[index]
+
+    for path in paths:
+        if len(set(path) & set(valid_path)) == 0:
+            if index == len(all_paths) - 1:
+                return True
+            else:
+                value = has_path_combination(all_paths, valid_path + path, index + 1)
+                if value == True: return True
+
+    return False
+
+def is_solvable_arukone(arukone: list, pairs: int):
     all_paths = []
 
-    for pair in pairs:
-        paths = find_first_occurrence(arukone, pair + 1)
+    for pair in range(1, pairs + 1):
+        row, col = find_first_occurrence(arukone, pair)
+        paths = find_path(arukone, [(pair, row, col)])
         all_paths.append(paths)
 
+    return has_path_combination(all_paths)
 
-paths = find_path([
+def generate_arukone(field_size: int, pairs: int) -> list:
+    if field_size < 5:
+        raise ValueError("Parameter field_size must be greater than 4")
+    
+    field = [[0 for _ in range(field_size)] for _ in range(field_size)]
+
+    for i in range(1, pairs + 1):
+        index_row_a = random.randint(0, field_size - 1)
+        index_item_a = random.randint(0, field_size - 1)
+
+        index_row_b = random.randint(0, field_size - 1)
+        index_item_b = random.randint(0, field_size - 1)
+
+        if field[index_row_a][index_item_a] == 0: 
+            field[index_row_a][index_item_a] = i
+        if field[index_row_b][index_row_b] == 0:
+            field[index_row_b][index_item_b] = i
+
+    if not is_solvable_arukone(field, pairs):
+        return generate_arukone(field_size, pairs)
+
+    return field
+
+print(is_solvable_arukone([
 [1, 0, 2, 4, 0, 0],
 [0, 0, 3, 0, 5, 0],
 [0, 0, 1, 0, 0, 0],
 [0, 0, 0, 0, 0, 4],
 [0, 2, 0, 0, 0, 0],
-[0, 0, 0, 3, 0, 5]], [(1, 0, 0)])
+[0, 0, 0, 3, 0, 5]], 5))
 
-for path in paths:
-    print(path)
+# print(is_solvable_arukone([
+# [1, 0, 0, 0, 0, 0, 0],
+# [0, 0, 0, 0, 0, 0, 0],
+# [0, 0, 0, 0, 0, 0, 0],
+# [2, 0, 0, 0, 0, 0, 2],
+# [0, 0, 0, 0, 0, 0, 0],
+# [0, 0, 0, 0, 0, 0, 0],
+# [0, 0, 0, 0, 0, 0, 1]], 2))
+
+for row in generate_arukone(6, 5):
+    print(row)
